4e91a0ae9b6ef279817871515e7cc15a76e5635e
Jorge Mena <isc.jorge.mena@gmail.com>
2022-10-17 16:46:02 -0500

    Avance en la maquetacion de los favoritos

--- containers/layout/menu.js
+++ containers/layout/menu.js
@@ -3,52 +3,40 @@ import { Dropdown, Menu, Space } from 'antd';
 import { useState } from 'react';
 import FormAdd from '../index/components/formAdd';
 import Image from 'next/image'
+import { useDispatch, useSelector } from "react-redux";
+
 
 const MenuHeader = () => {
     const [openModal, setOpenModal] = useState(false);
+    const dispatch = useDispatch();
+    const homeData = useSelector((state) => state.homeData);
+    const { favorites } = homeData;
+
     const labels = {
         favorites: 'Favoritos',
         add: 'Agregar'
     }
 
-    const menu = (
-        <Menu className='menu-favorites'
-            items={[
-                {
-                    key: '1',
-                    label: (
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                            1st menu item
-                        </a>
-                    ),
-                },
-                {
-                    key: '2',
-                    label: (
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-                            2nd menu item (disabled)
-                        </a>
-                    ),
-                    icon: <SmileOutlined />,
-                    disabled: true,
-                },
-                {
-                    key: '3',
-                    label: (
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-                            3rd menu item (disabled)
-                        </a>
-                    ),
-                    disabled: true,
-                },
-                {
-                    key: '4',
-                    danger: true,
-                    label: 'a danger item',
-                },
-            ]}
-        />
-    );
+    const generateItemsMenu = () => {
+        let items = favorites.map((favorite, index) => {
+            return {
+                key: favorite.id,
+                label: (
+                    <div>
+                        <div><Image src={favorite.image} width={34} height={34} /></div>
+                        <div>{favorite.name}</div>
+                        <div><Image src='/images/icons/trash.png' width={24} height={24} /></div>
+                    </div>
+                ),
+            }
+        })
+        console.log(items);
+        return (<>
+            <Menu className='menu-favorites'
+                items={items}
+            />
+        </>)
+    }
 
     const closeModal = () => {
         setOpenModal(false);
@@ -57,7 +45,7 @@ const MenuHeader = () => {
     return (
         <div className='menu'>
             <div className='favorites'>
-                <Dropdown overlay={menu}>
+                <Dropdown overlay={generateItemsMenu}>
                     <a onClick={(e) => e.preventDefault()}>
                         {labels.favorites} <Image className='icon-mark' src='/images/icons/bookmark_fill.png' width={16} height={16} />
                     </a>
